70797b46888afb0bc2b42bfd100fbec1da061c4d
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2022-09-05 10:12:49 +0300

    create auth page

---
+++ src/components/auth/AuthForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthForm = () => {
+    return (
+        <div>
+            <a href="http://localhost:8082/oauth/login/google">login with google</a>
+        </div>
+    )
+}
+
+export default AuthForm
