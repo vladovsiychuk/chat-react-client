1cf03756f05bc26f0b7e6206e9fd97dea7ca18a2
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2022-09-05 10:14:00 +0300

    redirect to auth page when user is not logged in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import ChatRoom from './components/ChatRoom'
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { getAccessToken } from './state/middleware/authMiddleware';
+import ChatRoom from './pages/ChatRoom';
+import AuthPage from './pages/AuthPage';
 
 const App = () => {
-  return (
-    <ChatRoom />
-  )
-}
+    const token = getAccessToken()
+
+    console.log(token)
+
+    return (
+        <Switch>
+            {!token && (
+                <Route path='/auth'>
+                    <AuthPage />
+                </Route>
+            )}
+            {!token && (
+                <Route path='/'>
+                    <Redirect to="/auth" />
+                </Route>
+            )}
+            <Route path="/" exact>
+                <ChatRoom />
+            </Route>
+        </Switch>
+    );
+};
 
 export default App;
 
