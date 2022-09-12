0e671d6ef094b77abb6f6a3a33720e26211dadb7
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2022-09-12 21:30:30 +0300

    refactor initial page

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -4,9 +4,25 @@ import GoogleButton from 'react-google-button'
 const AuthForm = () => {
     return (
         <div>
-            <GoogleButton
-                onClick={() => { window.location.replace("http://localhost:8082/oauth/login/google"); }}
-            />
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginTop: '300px',
+            }}>
+                <h1>
+                    Welcome to your business chat!
+                </h1>
+            </div>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <GoogleButton
+                    onClick={() => { window.location.replace("http://localhost:8082/oauth/login/google"); }}
+                />
+            </div>
         </div>
     )
 }
