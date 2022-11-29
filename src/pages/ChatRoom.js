b0ab2901221e426ccdd69f6f1596c79e126b1f1f
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2022-11-29 13:16:03 +0200

    uncomment after finishing test grids

--- src/pages/ChatRoom.js
+++ src/pages/ChatRoom.js
@@ -15,61 +15,82 @@ function ChatRoom({ loadChats, currentUserConnected, currentUserJoined, addNewMe
     const [tab, setTab] = useState(null);
     const [message, setMessage] = useState("")
 
-    const chats = useSelector(state => state.chats.chats)
-    const chatsAsync = useSelector(state => state.chats.async)
-    const currentUser = useSelector(state => state.users.currentUser)
-
-
-    useEffect(() => {
-        loadChats()
-        connect()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    const connect = () => {
-        let Sock = new SockJS('http://localhost:8080/ws');
-        stompClient = over(Sock);
-        stompClient.connect({}, onConnected, onError);
-    };
-
-    const onConnected = () => {
-        currentUserConnected(true);
-        stompClient.subscribe('/chatroom/public', onMessageReceived);
-        stompClient.subscribe('/user/' + currentUser.id + '/private', onPrivateMessage);
-        userJoin();
-    };
-
-    const userJoin = () => {
-        const chatMessage = {
-            sender: currentUser.data.id,
-            status: 'JOIN'
-        };
-        stompClient.send('/app/message', {}, JSON.stringify(chatMessage));
-    };
-
-    const onMessageReceived = (payload) => {
-        const payloadData = JSON.parse(payload.body);
-        switch (payloadData.status) {
-            case 'JOIN':
-                currentUserJoined(true)
-                break;
-            default:
-                return;
+    const chats = [
+        {
+            companion: "companion-A",
+            messages: [
+                {
+                    sender: "companion-A",
+                    receiver: "my-user",
+                    message: "message from companion-A to me",
+                },
+                {
+                    sender: "my-user",
+                    receiver: "companion-A",
+                    message: "message from me to companion-A",
+                }
+            ]
+        },
+    ]
+    const chatsAsync = {isLoading: false}
+    const currentUser = {
+        socketData: {connected: true},
+        data: {
+            id: "my-user"
         }
-    };
+    }
 
-    const onPrivateMessage = (payload) => {
-        const payloadData = JSON.parse(payload.body);
-        const sender = payloadData.sender
-        const receiver = payloadData.receiver
-        const companion = sender === currentUser.data.id ? receiver : sender
 
-        addNewMessage(payloadData, companion)
-    };
+    // useEffect(() => {
+    //     loadChats()
+    //     connect()
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, []);
 
-    const onError = (err) => {
-        console.log(err);
-    };
+    // const connect = () => {
+    //     let Sock = new SockJS('http://localhost:8080/ws');
+    //     stompClient = over(Sock);
+    //     stompClient.connect({}, onConnected, onError);
+    // };
+    //
+    // const onConnected = () => {
+    //     currentUserConnected(true);
+    //     stompClient.subscribe('/chatroom/public', onMessageReceived);
+    //     stompClient.subscribe('/user/' + currentUser.id + '/private', onPrivateMessage);
+    //     userJoin();
+    // };
+
+    // const userJoin = () => {
+    //     const chatMessage = {
+    //         sender: currentUser.data.id,
+    //         status: 'JOIN'
+    //     };
+    //     stompClient.send('/app/message', {}, JSON.stringify(chatMessage));
+    // };
+
+    // const onMessageReceived = (payload) => {
+    //     const payloadData = JSON.parse(payload.body);
+    //     switch (payloadData.status) {
+    //         case 'JOIN':
+    //             currentUserJoined(true)
+    //             break;
+    //         default:
+    //             return;
+    //     }
+    // };
+
+    // const onPrivateMessage = (payload) => {
+    //     const payloadData = JSON.parse(payload.body);
+    //     const sender = payloadData.sender
+    //     const receiver = payloadData.receiver
+    //     const companion = sender === currentUser.data.id ? receiver : sender
+    //
+    //     addNewMessage(payloadData, companion)
+    // };
+
+    // const onError = (err) => {
+    //     console.log(err);
+    // };
 
     const handleMessage = (event) => {
         const { value } = event.target; //create message state
@@ -77,25 +98,25 @@ function ChatRoom({ loadChats, currentUserConnected, currentUserJoined, addNewMe
     };
 
     const sendPrivateValue = () => {
-        if (stompClient) {
-            const chatMessage = {
-                sender: currentUser.data.id,
-                receiver: tab,
-                date: Date.now(),
-                message: message,
-                status: 'MESSAGE'
-            };
-
-            stompClient.send('/app/private-message', {}, JSON.stringify(chatMessage));
-
-            const sender = chatMessage.sender
-            const receiver = chatMessage.receiver
-            const companion = sender === currentUser.data.id ? receiver : sender
-
-            addNewMessage(chatMessage, companion)
-
-            setMessage("")
-        }
+        // if (stompClient) {
+        //     const chatMessage = {
+        //         sender: currentUser.data.id,
+        //         receiver: tab,
+        //         date: Date.now(),
+        //         message: message,
+        //         status: 'MESSAGE'
+        //     };
+        //
+        //     stompClient.send('/app/private-message', {}, JSON.stringify(chatMessage));
+        //
+        //     const sender = chatMessage.sender
+        //     const receiver = chatMessage.receiver
+        //     const companion = sender === currentUser.data.id ? receiver : sender
+        //
+        //     addNewMessage(chatMessage, companion)
+        //
+        //     setMessage("")
+        // }
     };
 
     return (
@@ -151,18 +172,18 @@ function ChatRoom({ loadChats, currentUserConnected, currentUserJoined, addNewMe
     );
 }
 
-ChatRoom.propTypes = {
-    loadChats: PropTypes.func.isRequired,
-    currentUserConnected: PropTypes.func.isRequired,
-    currentUserJoined: PropTypes.func.isRequired,
-    addNewMessage: PropTypes.func.isRequired,
-};
+// ChatRoom.propTypes = {
+//     loadChats: PropTypes.func.isRequired,
+//     currentUserConnected: PropTypes.func.isRequired,
+//     currentUserJoined: PropTypes.func.isRequired,
+//     addNewMessage: PropTypes.func.isRequired,
+// };
 
-const mapDispatchToProps = {
-    loadChats: loadChats,
-    currentUserConnected: currentUserConnected,
-    currentUserJoined: currentUserJoined,
-    addNewMessage: addNewMessage
-};
+// const mapDispatchToProps = {
+//     loadChats: loadChats,
+//     currentUserConnected: currentUserConnected,
+//     currentUserJoined: currentUserJoined,
+//     addNewMessage: addNewMessage
+// };
 
-export default connect(null, mapDispatchToProps)(ChatRoom);
+export default connect(null, null)(ChatRoom);
