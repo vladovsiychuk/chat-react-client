92681267b16891d3c8215d395d70a423f4729857
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-03-12 23:36:06 +0200

    truncate usernamas in search list

--- src/components/user/UserSearchItem.js
+++ src/components/user/UserSearchItem.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { ListItemButton, ListItemAvatar, Avatar, Divider } from '@mui/material';
+import { ListItemButton, ListItemAvatar, Avatar, Divider, ListItemText } from '@mui/material';
 
-function UserSearchItem(props) {
-    const { user, tab, handleSearchUsersClick } = props;
+function UserSearchItem({ user, tab, handleSearchUsersClick }) {
 
     return (
         <React.Fragment>
@@ -19,10 +18,21 @@ function UserSearchItem(props) {
                             .toUpperCase()}
                     </Avatar>
                 </ListItemAvatar>
+                <ListItemText
+                    primary={truncateString(user.email)}
+                />
             </ListItemButton>
             <Divider></Divider>
         </React.Fragment>
     );
 }
+
+function truncateString(str) {
+    if (str.length > 25) {
+        str = str.substring(0, 22) + '...';
+    }
+    return str;
+}
+
 
 export default UserSearchItem;
