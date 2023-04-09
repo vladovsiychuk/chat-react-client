4d29deeaca798f3c0ff51ae72e47216d3bab3d6a
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-04-09 17:32:46 +0300

    Revert "test websockets"
    This reverts commit 8f26828ba3f192bdcd63deaa4f6d24f4b3debe94.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './state/store';
-import TestComponent from './TestComponent';
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            {/*<App />*/}
-            <TestComponent />
+            <App />
         </BrowserRouter>
     </Provider>,
     document.getElementById('root')
