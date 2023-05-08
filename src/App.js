f3c3edc03bd5aa3a14844c8a73dd95bd00d0fa9b
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-05-08 20:46:26 +0300

    formatting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { useEffect } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { getAccessToken } from './state/middleware/authMiddleware';
+import React, {useEffect} from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import {getAccessToken} from './state/middleware/authMiddleware';
 import ChatRoom from './pages/ChatRoom';
 import AuthPage from './pages/AuthPage';
 import PropTypes from 'prop-types';
-import { connect, useSelector } from 'react-redux';
-import { createCurrentUser, loadCurrentUser } from './state/users/actions';
+import {connect, useSelector} from 'react-redux';
+import {createCurrentUser, loadCurrentUser} from './state/users/actions';
 
-const App = ({ loadCurrentUser, createCurrentUser }) => {
+const App = ({loadCurrentUser, createCurrentUser}) => {
     const token = getAccessToken()
-    const { currentUserIsFound } = useSelector(state => state.users.async)
-    const { isLoading } = useSelector(state => state.users.async)
+    const {currentUserIsFound} = useSelector(state => state.users.async)
+    const {isLoading} = useSelector(state => state.users.async)
 
     useEffect(() => {
-        console.log("Inside App.useEffect()")
-
         if (isLoading !== true) {
             if (!!token && currentUserIsFound == null) {
                 loadCurrentUser()
@@ -31,17 +29,17 @@ const App = ({ loadCurrentUser, createCurrentUser }) => {
         <Switch>
             {!token && (
                 <Route path='/auth'>
-                    <AuthPage />
+                    <AuthPage/>
                 </Route>
             )}
             {!token && (
                 <Route path='/'>
-                    <Redirect to="/auth" />
+                    <Redirect to="/auth"/>
                 </Route>
             )}
             {!!currentUserIsFound && (
                 <Route path="/" exact>
-                    <ChatRoom />
+                    <ChatRoom/>
                 </Route>
             )}
         </Switch>
